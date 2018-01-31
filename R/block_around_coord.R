8650f180b023393769d9cefe5a924dd9add5a842
MHaringa <mtharinga@gmail.com>
2018-01-31 12:52:42 +0100

    update

--- R/block_around_coord.R
+++ R/block_around_coord.R
@@ -1,21 +1,24 @@
 
 #' Title
 #'
-#' @param lon
-#' @param lat
-#' @param radius
+#' @param lon Longitude of point(s), in degrees.
+#' @param lat Latitude of point(s), in degrees.
+#' @param radius Major (equatorial) radius (default is meters) of the ellipsoid. The default value is for WGS84.
 #'
 #' @return
 #' @export
 #'
 #' @examples
 block_around_coord <- function(lon = lon, lat = lat, radius = 200){
+
   tibble(lon = lon, lat = lat) %>%
     mutate(west = geosphere::destPoint(c(lon, lat), 270, radius)[[1]]) %>%
     mutate(east = geosphere::destPoint(c(lon, lat), 90, radius)[[1]]) %>%
     mutate(south = geosphere::destPoint(c(lon, lat), 180, radius)[[2]]) %>%
     mutate(north = geosphere::destPoint(c(lon, lat), 360, radius)[[2]])
 }
+
+
 
 
 
--- R/coord_in_circle.R
+++ R/coord_in_circle.R
@@ -1,16 +1,18 @@
 #' Coordinates in circle
 #'
-#' @param lon
-#' @param lat
-#' @param radius
+#' @param lon Longitude of centre point, in degrees.
+#' @param lat Latitude of centre point, in degrees.
+#' @param radius Major (equatorial) radius (default is meters) of the ellipsoid. The default value is for WGS84.
 #'
-#' @return
+#' @return A data.frame of coordinates within \code{radius} around \code{(lon, lat)}.
 #' @export
 #'
 #' @examples
+#' #
+#' coord_in_circle(Groningen, lon = 6.5203, lat = 53.24007, radius = 200)
 coord_in_circle <- function(data, lon = lon, lat = lat, radius = 200){
 
-  if (all(names(data) %in% c("lon", "lat")) == FALSE) stop("Data does not have latitude and longitude information included.")
+  if (sum(names(data) %in% c("lon", "lat")) < 2) stop("Data does not have latitude and longitude information included.")
 
   centre <- data.frame(lon = lon, lat = lat)
 
@@ -19,10 +21,20 @@ coord_in_circle <- function(data, lon = lon, lat = lat, radius = 200){
   data %>%
     filter(., lon > block$west & lon < block$east) %>%
     filter(., lat > block$south & lat < block$north) %>%
-    mutate(distanceFromCentre = purrr::pmap_dbl(., function(lon, lat)
+    mutate(distanceFromCentre = purrr::pmap_dbl(., function(lon, lat, ...)
       geosphere::distHaversine(c(lon, lat), centre))) %>%
-    filter(., distanceFromCentre < radius)
+    filter(., distanceFromCentre < radius) %>%
+    arrange(distanceFromCentre)
 }
+
+
+
+
+
+
+
+
+
 
 
 
