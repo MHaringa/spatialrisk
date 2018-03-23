f3babbddaba8935afed7a2179b23a00ceae62975
MHaringa <mtharinga@gmail.com>
2018-03-23 12:14:26 +0100

    name lonlat columns

--- R/concentration.R
+++ R/concentration.R
@@ -11,15 +11,23 @@
 #' concentration(Groningen[1:10, ], amount, radius = 1000)
 #'
 #' @export
-concentration <- function(data, value, radius = 200){
+concentration <- function(data, value, radius = 200, lon = lon, lat = lat){
   dt <- data.table(data)
+
   value <- deparse(substitute(value))
-  setnames(dt, value, "value")
+  lon <- deparse(substitute(lon))
+  lat <- deparse(substitute(lat))
+
+  setnames(dt, c(value, lon, lat), c("value", "lon", "lat"))
+
   concentration <- dt[, concentration := sum_in_circle(dt, value = value, lon_center = lon, lat_center = lat, radius = radius),
                       by = 1:nrow(dt)][order(-concentration)]
-  setnames(concentration, "value", value)
+
+  setnames(concentration, c("value", "lon", "lat"), c(value, lon, lat))
   return(concentration)
 }
+
+
 
 
 
