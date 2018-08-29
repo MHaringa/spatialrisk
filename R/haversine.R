3b548bffdefee3678e5df727fe2681d41a035aa6
MHaringa <mtharinga@gmail.com>
2018-08-29 09:59:45 +0200

    haversine

--- R/haversine.R
+++ R/haversine.R
@@ -7,8 +7,9 @@
 #'
 #' @return Distance in meters
 #' @description The shortest distance between two points (i.e., the 'great-circle-distance' or 'as the crow flies'),
-#' according to the 'haversine method'. This method assumes a spherical earth, ignoring ellipsoidal effects. Note that this version is a copy
-#' of geosphere, but without the matrix conversion. A quick benchmark showed it to be a non-insignificant improvement.
+#' according to the 'haversine method'. This method assumes a spherical earth, ignoring ellipsoidal effects. Note
+#' that this version is implemented in C++. A quick benchmark to the version of geosphere showed it to be a non-insignificant speed enhancement.
+#'
 #'
 #' @details The Haversine ('half-versed-sine') formula was published by R.W. Sinnott in 1984, although it has been known for much longer.
 #'
@@ -21,15 +22,7 @@ haversine <- function(lat_from, lon_from, lat_to, lon_to){
   if( lat_from > 90 | lat_to > 90) warning('latitude > 90')
   if( lon_from > 360 | lon_to > 360) warning('longitude > 360')
 
-  toRad <- pi / 180
-  lat_to <- lat_to * toRad
-  lat_from <- lat_from * toRad
-  lon_to <- lon_to * toRad
-  lon_from <- lon_from * toRad
-  dLat <- (lat_to - lat_from)
-  dLon <- (lon_to - lon_from)
-  a <- (sin(dLat / 2) ^ 2) + (cos(lat_from) * cos(lat_to)) * (sin(dLon / 2) ^ 2)
-  dist <- 2 * atan2(sqrt(a), sqrt(1 - a)) * 6378137
+  dist <- haversine_cpp(lat_from, lon_from, lat_to, lon_to)
   return(dist)
 }
 
