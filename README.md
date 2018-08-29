828b13e5c4db67b2373a83b4ff6fa56fad5bfae3
MHaringa <mtharinga@gmail.com>
2018-08-29 08:35:19 +0200

    readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # spatialrisk
 
-spatialrisk is an R-package for spatial risk calculations. In particular, it can be used to determine concentration risk. The package is currently work-in-progress and contributions are welcome.
+spatialrisk is an R-package for spatial risk calculations. In particular, it can be used to determine concentration risk in the context of Solvency II. The package offers an effective approach to calculate the ‘standard formula’ under Solvency II. The ‘standard formula’ under Solvency II asks companies to report their largest fire concentration in respect of the fire peril within a radius of 200m. This is the maximum gross sum insured of the set of buildings fully or partly located within this radius. The package is currently work-in-progress and contributions are welcome.
 
 The quickest way to get the development version of the package running locally is using devtools:
 
--- src/haversine_cpp.cpp
+++ src/haversine_cpp.cpp
@@ -5,8 +5,8 @@ using namespace Rcpp;
 double haversine_cpp(double lat1, double long1,
                      double lat2, double long2){
   double earth_radius = 6371;
-  double deg_to_rad = 0.017453292519943295769236907684886; // i.e. pi/180 (multiplication is faster than division)
-  double delta_phi = (lat2 - lat1) * deg_to_rad; 
+  double deg_to_rad = 0.0174532925199432957; // i.e. pi/180 (multiplication is faster than division)
+  double delta_phi = (lat2 - lat1) * deg_to_rad;
   double delta_lambda = (long2 - long1) * deg_to_rad;
   double phi1 = lat1 * deg_to_rad;
   double phi2 = lat2 * deg_to_rad;
@@ -20,52 +20,52 @@ double haversine_cpp(double lat1, double long1,
 
 // [[Rcpp::export]]
 DataFrame haversine_loop_cpp(DataFrame x, double lat_center, double lon_center, double radius = 200) {
-  
+
   // extracting each column into a vector
   NumericVector number = x["number"];
   CharacterVector postalcode = x["postal_code"];
   NumericVector lon = x["lon"];
   NumericVector lat = x["lat"];
   NumericVector amount = x["amount"];
-  
+
   // create block around center point
   int circumference_earth_in_meters = 40075000;
   double one_lat_in_meters = circumference_earth_in_meters * 0.002777778;  // 0.002777778 is used instead of 1/360;
-  double one_lon_in_meters = circumference_earth_in_meters * cos(lat_center * 0.01745329) * 0.002777778; 
+  double one_lon_in_meters = circumference_earth_in_meters * cos(lat_center * 0.01745329) * 0.002777778;
   double south_lat = lat_center - (radius + 5) / one_lat_in_meters;
   double north_lat = lat_center + (radius + 5)  / one_lat_in_meters;
   double west_lon = lon_center - (radius + 5)  / one_lon_in_meters;
   double east_lon = lon_center + (radius + 5)  / one_lon_in_meters;
-  
+
   // apply "pre-subsetting" before using haversine method
   int n = x.nrows();
   LogicalVector ind_block(n);
-  
+
   for ( int i = 0; i < n; i++ ){
     ind_block[i] = (lon[i] < east_lon & lon[i] > west_lon & lat[i] > south_lat & lat[i] < north_lat);
   }
-  
-  // create new data.frame based on "pre-subsetting"  
+
+  // create new data.frame based on "pre-subsetting"
   NumericVector number_sub = number[ind_block];
   CharacterVector postalcode_sub = postalcode[ind_block];
   NumericVector lat_sub = lat[ind_block];
   NumericVector lon_sub = lon[ind_block];
   NumericVector amount_sub = amount[ind_block];
-  
+
   int n1 = amount_sub.size();
-  
+
   // apply haversine method to find points within radius from center
   NumericVector result(n1);
   for ( int i = 0; i < n1; ++i ) {
      result[i] = haversine_cpp(lat_center, lon_center, lat_sub[i], lon_sub[i]);
   }
-  
+
   // create indicator whether coordinates are within radius
   LogicalVector ind_radius(n1);
   for (int i = 0; i < n1; i++){
     ind_radius[i] = (result[i] < radius);
   }
-  
+
   // create a new data frame
   DataFrame NDF = DataFrame::create(Named("number") = number_sub[ind_radius],
                                     Named("postal_code") = postalcode_sub[ind_radius],
@@ -80,25 +80,25 @@ DataFrame haversine_loop_cpp(DataFrame x, double lat_center, double lon_center, 
 
 // [[Rcpp::export]]
 DataFrame concentration_loop_cpp(DataFrame sub, DataFrame ref, double radius = 200) {
-  
+
   // extracting each column into a vector
   IntegerVector number = sub["number"];
   CharacterVector postalcode = sub["postal_code"];
   NumericVector amount = sub["amount"];
   NumericVector lon = sub["lon"];
   NumericVector lat = sub["lat"];
-  
+
   // define length of loop and create output vector
   int n = sub.nrows();
   NumericVector cumulation(n);
-  
+
   // determine cumulation per row
   for ( int i = 0; i < n; ++i ) {
     DataFrame result = haversine_loop_cpp(ref, lat[i], lon[i], radius);
     NumericVector amount = result["amount"];
     cumulation[i] = sum(amount);
   }
-  
+
   // create a new data frame
   DataFrame NDF = DataFrame::create(Named("number") = number,
                                     Named("postal_code") = postalcode,
