0b9d01bde79f1e9e349a834debf05a95e00ed46c
MHaringa <mtharinga@gmail.com>
2018-08-31 11:48:05 +0200

    documentation

--- R/concentration.R
+++ R/concentration.R
@@ -1,17 +1,20 @@
 #' Concentration risk
 #'
-#' @param sub data.frame of locations to calculate concentration risk for. The data.frame should include at least columns for longitude, latitude and value of interest to summarize.
-#' @param full data.frame of full portfolio. The data.frame should include at least columns for longitude, latitude and value of interest to summarize.
+#' @param sub data.frame of locations to calculate concentration risk for.
+#' @param full data.frame to find the locations within radius \code{r} from locations in \code{sub}.
 #' @param lon_sub Column name with longitude (lon is default).
 #' @param lat_sub Column name with latitude (lat is default).
-#' @param lon_full Column name with longitude in the full data (lon is default).
-#' @param lat_full Column name with latitude in the full data (lat is default).
-#' @param value_full Column name with value in the full data (value is default).
+#' @param lon_full Column name with longitude in \code{full} (lon is default).
+#' @param lat_full Column name with latitude in \code{full} (lat is default).
+#' @param value Column name with value in \code{full}.
 #' @param radius Radius (in meters) (default is 200m).
 #' @param display_progress Show progress bar (TRUE/FALSE).
 #'
 #' @description The points (e.g. addresses) with the highest concentrations. In the context of the 'standard formula' under Solvency II,
 #' this is the maximum gross sum insured of the set of buildings fully or partly located within this radius.
+#'
+#' @details \code{sub} should include at least columns for longitude and latitude.
+#' @details \code{full} should include at least columns for longitude, latitude and value of interest to summarize.
 #'
 #' @return A data.frame equal to data.frame \code{sub} including an extra column \code{concentration}.
 #'
@@ -28,19 +31,18 @@
 #' @export
 concentration <- function(sub, full,
                           lon_sub = lon, lat_sub = lat,
-                          lon_full = lon, lat_full = lat, value_full = value,
+                          lon_full = lon, lat_full = lat, value,
                           radius = 200, display_progress = TRUE){
 
   # Turn into character vector
   lon_sub <- deparse(substitute(lon_sub))
   lat_sub <- deparse(substitute(lat_sub))
-
   lon_full <- deparse(substitute(lon_full))
   lat_full <- deparse(substitute(lat_full))
-  value_full <- deparse(substitute(value_full))
+  value <- deparse(substitute(value))
 
   sub_df <- data.frame("lon" = sub[[lon_sub]], "lat" = sub[[lat_sub]])
-  full_df <- data.frame("lon" = full[[lon_full]], "lat" = full[[lat_full]], "value" = full[[value_full]])
+  full_df <- data.frame("lon" = full[[lon_full]], "lat" = full[[lat_full]], "value" = full[[value]])
 
   concentration_df <- concentration_loop_cpp(sub_df, full_df, radius, display_progress)
 
---
+++ cran-comments.md
@@ -0,0 +1,19 @@
+## Test environments
+* local OS X install, R 3.5.1
+* win-builder (devel and release)
+
+## R CMD check results
+There were no ERRORs or WARNINGs. 
+
+There was 1 NOTE:
+
+* checking CRAN incoming feasibility ... NOTE
+  Maintainer: 'Martin Haringa <mtharinga@gmail.com>'
+
+  New submission
+
+## Downstream dependencies
+I have also run R CMD check on downstream dependencies of spatialrisk.
+All packages that I could install passed.
+
+
