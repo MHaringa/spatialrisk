6ed20b95a38d30aeaf257d82dce4b570be362c1b
MHaringa <mtharinga@gmail.com>
2019-04-29 13:32:22 +0200

    palette

--- R/map_ggplot2.R
+++ R/map_ggplot2.R
@@ -7,6 +7,8 @@
 #' @param n number of clusters (default is 7)
 #' @param dig.lab number of digits in legend (default is 2)
 #' @param legend_title title of legend
+#' @param option a character string indicating the colormap option to use. Four options are available: "magma" (or "A"), "inferno" (or "B"), "plasma" (or "C"), "viridis" (or "D", the default option) and "cividis" (or "E").
+#' @param direction Sets the order of colors in the scale. If 1, the default, colors are ordered from darkest to lightest. If -1, the order of colors is reversed.
 #'
 #' @return ggplot map
 #' @export choropleth_ggplot2
@@ -21,7 +23,7 @@
 #' @examples
 #' test <- choropleth_sf(nl_postcode2, insurance, sum(amount, na.rm = TRUE))
 #' choropleth_ggplot2(test)
-choropleth_ggplot2 <- function(sf_object, value = output, n = 7, dig.lab = 2, legend_title = "Class"){
+choropleth_ggplot2 <- function(sf_object, value = output, n = 7, dig.lab = 2, legend_title = "Class", option = "D", direction = 1){
 
   value <- deparse(substitute(value))
   vector_value <- sf_object[[value]]
@@ -36,7 +38,7 @@ choropleth_ggplot2 <- function(sf_object, value = output, n = 7, dig.lab = 2, le
       ggplot(sf_object) +
         geom_sf(aes(fill = clustering), size = .1, color = "grey85")  +
         coord_sf(datum = NA) +
-        scale_fill_viridis_d() +
+        scale_fill_viridis_d(direction = direction, option = option) +
         theme_void() +
         labs(fill = legend_title)
     },
@@ -45,7 +47,7 @@ choropleth_ggplot2 <- function(sf_object, value = output, n = 7, dig.lab = 2, le
       ggplot(sf_object) +
         geom_sf(aes(fill = vector_value), size = .1, color = "grey85")  +
         coord_sf(datum = NA) +
-        scale_fill_viridis_c() +
+        scale_fill_viridis_c(direction = direction, option = option) +
         theme_void() +
         labs(fill = legend_title)
     })
