84af9681e5605d88e2e7ba6ada6fa66ebd87929b
MHaringa <mtharinga@gmail.com>
2019-06-03 15:13:08 +0200

    ignore

--- inst/spatialrisk/server.R
+++ inst/spatialrisk/server.R
@@ -2,9 +2,6 @@
 # This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
 #
 
 library(shiny)
